7c61a0d60c9874f0907f4b5b4dce346f71c9e064
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2022-09-20 22:27:51 -0300

    feat: turn typesDict an inline var

--- lua/cmp-conventionalcommits/init.lua
+++ lua/cmp-conventionalcommits/init.lua
@@ -1,43 +1,50 @@
 local source = {}
 
-local typesDict = {}
-typesDict['build'] = {
-  label = 'build',
-  documentation = 'Changes that affect the build system or external dependencies',
-}
-typesDict['chore'] = {
-  label = 'chore',
-  documentation = 'Other changes that dont modify src or test files',
-}
-typesDict['ci'] = {
-  label = 'ci',
-  documentation = 'Changes to our CI configuration files and scripts',
-}
-typesDict['docs'] = {
-  label = 'docs',
-  documentation = 'Documentation only changes',
-}
-typesDict['feat'] = {label = 'feat', documentation = 'A new feature'}
-typesDict['fix'] = {label = 'fix', documentation = 'A bug fix'}
-typesDict['perf'] = {
-  label = 'perf',
-  documentation = 'A code change that improves performance',
-}
-typesDict['refactor'] = {
-  label = 'refactor',
-  documentation = 'A code change that neither fixes a bug nor adds a feature',
-}
-typesDict['revert'] = {
-  label = 'revert',
-  documentation = 'Reverts a previous commit',
-}
-typesDict['style'] = {
-  label = 'style',
-  documentation = 'Changes that do not affect the meaning of the code',
-}
-typesDict['test'] = {
-  label = 'test',
-  documentation = 'Adding missing tests or correcting existing tests',
+local typesDict = {
+  build = {
+    label = 'build',
+    documentation = 'Changes that affect the build system or external dependencies',
+  },
+  chore = {
+    label = 'chore',
+    documentation = 'Other changes that dont modify src or test files',
+  },
+  ci = {
+    label = 'ci',
+    documentation = 'Changes to our CI configuration files and scripts',
+  },
+  docs = {
+    label = 'docs',
+    documentation = 'Documentation only changes',
+  },
+  feat = {
+    label = 'feat',
+    documentation = 'A new feature'
+  },
+  fix = {
+    label = 'fix',
+    documentation = 'A bug fix'
+  },
+  perf = {
+    label = 'perf',
+    documentation = 'A code change that improves performance',
+  },
+  refactor = {
+    label = 'refactor',
+    documentation = 'A code change that neither fixes a bug nor adds a feature',
+  },
+  revert = {
+    label = 'revert',
+    documentation = 'Reverts a previous commit',
+  },
+  style = {
+    label = 'style',
+    documentation = 'Changes that do not affect the meaning of the code',
+  },
+  test = {
+    label = 'test',
+    documentation = 'Adding missing tests or correcting existing tests',
+  }
 }
 
 function source.new()
